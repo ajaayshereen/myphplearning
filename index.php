29d05d38dbecdb8b35befdd3c39c484decb62069
OpenShift guest <ajaayshereen@hotmail.com>
2013-03-02 09:16:07 -0500

    added index and variable

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML>
+<HEAD>
+<TITLE>Index of pages</TITLE>
+</HEAD>
+<BODY>
+
+Basic <br />
+<ul>
+    <li><a href="php/hello.php">Hello World</a> </li>
+    <li><a href="php/variable.php">Variables</a> </li>
+    
+</ul>
+
+</BODY>
+</HTML>
