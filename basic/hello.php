bc94500cbc4f5adecb4ca8e564662107b136a541
Ajaay Shereen <ajaayshereen@yahoo.com>
2013-03-03 12:03:51 +0530

    updating folder structure

---
+++ basic/hello.php
@@ -0,0 +1,14 @@
+<HTML>
+<HEAD>
+<TITLE>Helo</TITLE>
+</HEAD>
+<BODY>
+
+<?php 
+
+echo "Hello World from php";
+
+?>
+
+</BODY>
+</HTML>
---
+++ basic/variable.php
@@ -0,0 +1,21 @@
+<HTML>
+<HEAD>
+<TITLE>Variable</TITLE>
+</HEAD>
+<BODY>
+
+<?php 
+
+$myVar = "Welcome to cloud";
+echo $myVar;
+
+$var1 = 23;
+$var2 = 42;
+$var3 = $var1 + $var2;
+
+echo $var1 . " + " . $var2 . " = " . $var3;
+
+?>
+
+</BODY>
+</HTML>
